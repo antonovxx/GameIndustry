a28396f96b5be03ef2077fae20c7f1a3fc399aee
antonovxx <80815609+antonovxx@users.noreply.github.com>
2023-12-04 00:27:00 +0300

    the third lesson is done. EventCallBacks.

--- GameIndustry/Models/Game.cs
+++ GameIndustry/Models/Game.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace GameIndustry.Models
 {
     public class Game
     {
+        public string Name { get; set; }
         
+        public string Genre { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }
     }
 }
